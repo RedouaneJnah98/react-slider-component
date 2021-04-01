85480ad9f5a63cb016366d3315ec28fb935e2bee
Redouane Jnah <redouanejnah2000@gmail.com>
2021-04-01 15:49:04 +0100

    basic setup

---
+++ src/components/Slider.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import data from '../data/data';
+
+const Slider = () => {
+  return <h3>slider component</h3>;
+};
+
+export default Slider;
---
+++ src/data/data.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    name: 'Tanya Sinclair',
+    job: 'UX Engineer',
+    info:
+      '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ” ',
+  },
+  {
+    name: 'John Tarkpor',
+    job: 'Junior Front-end Developer',
+    info:
+      '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ” ',
+  },
+];
+
+export default data;
